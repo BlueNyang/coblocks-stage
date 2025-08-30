968c5ca6e629b707206dc008d56173b7286ca8e4
BlueNyang <xpsj20@gmail.com>
2025-08-30 21:43:44 +0900

    help me

--- packages/core/src/errors/workerError.ts
+++
@@ -1,7 +0,0 @@
-/** Error thrown when an object cannot be dropped */
-export class WorkerNotInitializedError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = "WorkerNotInitializedError";
-  }
-}
